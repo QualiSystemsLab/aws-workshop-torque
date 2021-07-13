e02d23c429fa17b37369a3ef721ea4afa06ad08b
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2021-07-13 14:45:11 +0300

    Update insert_data.sh

--- terraform/aws-document-db/insert_data.sh
+++ terraform/aws-document-db/insert_data.sh
@@ -12,7 +12,6 @@ echo 'Install MongoDB'
 apt-get update && apt-get install -y gnupg2
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-apt-get update -y
 apt-get install -y mongodb-org
 echo "mongodb-org hold" | dpkg --set-selections
 echo "mongodb-org-server hold" | dpkg --set-selections
