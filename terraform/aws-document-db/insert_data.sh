443bc3205de287350af2349a528aabcaff3a03f6
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2021-07-14 16:11:38 +0300

    Update insert_data.sh

--- terraform/aws-document-db/insert_data.sh
+++ terraform/aws-document-db/insert_data.sh
@@ -10,7 +10,7 @@ DATA="$6"
 # install mongoimport cli tool
 echo 'Install MongoDB'
 #apt-get update -y && apt-get install gnupg
-apt-get install gnupg
+apt-get install -y gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | apt-key add -
 
 #wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
