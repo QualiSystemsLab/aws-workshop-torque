3af919c9b8343435aabbc1dd7bee5af7a42e6119
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2021-07-15 15:02:31 +0300

    Update insert_data.sh

--- terraform/aws-document-db/insert_data.sh
+++ terraform/aws-document-db/insert_data.sh
@@ -9,6 +9,7 @@ DATA="$6"
 
 # install mongoimport cli tool
 echo 'Install MongoDB'
+apt-get install -y gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 apt-get update -y
