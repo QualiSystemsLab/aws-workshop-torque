aab8ba6e601a0dff9c803e213c9aee579a690850
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2021-07-15 14:35:35 +0300

    Update insert_data.sh

--- terraform/aws-document-db/insert_data.sh
+++ terraform/aws-document-db/insert_data.sh
@@ -13,14 +13,16 @@ echo 'Install MongoDB'
 apt remove mongodb-org
 apt-get install -y gnupg
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.0.asc | apt-key add -
+
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
 
 #wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
 # apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 #echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 
 apt-get update -y
 
