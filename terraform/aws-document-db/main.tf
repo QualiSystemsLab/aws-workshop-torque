62ddbbc0980f5c78589a0906b6cc9da0caf7ee20
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2021-07-20 13:04:45 +0300

    Update main.tf

--- terraform/aws-document-db/main.tf
+++ terraform/aws-document-db/main.tf
@@ -99,15 +99,11 @@ resource "aws_docdb_cluster" "default" {
     }
 }
 
-locals {
-    DATA = "[{"email":"admin","password":"admin"}]"
-}
-
 resource "null_resource" "insert_data" {
     count = "${var.INSERT_DATA ? 1 : 0}"
 
     provisioner "local-exec" {
-        command = "chmod 777 insert_data.sh && ./insert_data.sh ${aws_docdb_cluster.default.endpoint} ${var.USERNAME} ${var.PASSWORD} ${var.DB_NAME} ${var.COLLECTION_NAME} ${local.DATA} ${aws_docdb_cluster_instance.cluster_instances[0].arn}"
+        command = "chmod 777 insert_data.sh && ./insert_data.sh ${aws_docdb_cluster.default.endpoint} ${var.USERNAME} ${var.PASSWORD} ${var.DB_NAME} ${var.COLLECTION_NAME} \"${var.DATA}\" ${aws_docdb_cluster_instance.cluster_instances[0].arn}"
         interpreter = ["/bin/bash", "-c"]
     }
 }
