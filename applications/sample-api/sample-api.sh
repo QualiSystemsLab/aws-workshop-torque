236bcb82b03a4b584195e4316b72a84357ce24e3
Rajeevranjan Sharma <raju.rajeev420@gmail.com>
2021-10-06 11:44:31 +0530

    Update sample-api.sh

--- applications/sample-api/sample-api.sh
+++ applications/sample-api/sample-api.sh
@@ -83,13 +83,13 @@ mkdir $ARTIFACTS_PATH/drop
 # tar -xvf $ARTIFACTS_PATH/sample-api.*.tar.gz -C $ARTIFACTS_PATH/drop/
 
 tar -xvf $ARTIFACTS_PATH/sample-api-0.0.1.tar.gz -C $ARTIFACTS_PATH/drop/
-tar -xvf $ARTIFACTS_PATH/sample_api-*.* -C $ARTIFACTS_PATH/drop/
+# tar -xvf $ARTIFACTS_PATH/sample_api-*.* -C $ARTIFACTS_PATH/drop/
 
 mkdir /var/sample-api/
 
 # tar -xvf $ARTIFACTS_PATH/drop/drop/sample-api.*.tar.gz -C /var/sample-api
 tar -xvf $ARTIFACTS_PATH/drop/drop/sample-api-0.0.1.tar.gz -C /var/sample-api
-tar -xvf $ARTIFACTS_PATH/drop/drop/sample_api-*.* -C /var/sample-api
+# tar -xvf $ARTIFACTS_PATH/drop/drop/sample_api-*.* -C /var/sample-api
 
 echo 'RELEASE_NUMBER='$RELEASE_NUMBER >> /etc/environment
 echo 'API_BUILD_NUMBER='$API_BUILD_NUMBER >> /etc/environment
