1500800044d6e747297a962740c863a3103e08f6
shirabendor-quali <76521367+shirabendor-quali@users.noreply.github.com>
2021-10-07 10:46:32 +0300

    Update promotions-manager-api-aws.sh

--- applications/promotions-manager-api/promotions-manager-api-aws.sh
+++ applications/promotions-manager-api/promotions-manager-api-aws.sh
@@ -4,11 +4,31 @@ echo '=============== Staring init script for Promotions Manager API ===========
 # save all env for debugging
 printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
 
-echo '==> Installing Node.js and NPM'
+echo '==> apt-get update'
+apt-get update -y
+
+echo '==> Instal curl'
+apt-get install curl -y
+
+echo '==> Installing node 10'
+sudo add-apt-repository -y -r ppa:chris-lea/node.js
+sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list
+sudo rm -f /etc/apt/sources.list.d/chris-lea-node_js-*.list.save
+KEYRING=/usr/share/keyrings/nodesource.gpg
+wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | sudo tee "$KEYRING" >/dev/null
+gpg --no-default-keyring --keyring "$KEYRING" --list-keys
+VERSION=node_10.x
+DISTRO="$(lsb_release -s -c)"
+echo "deb [signed-by=$KEYRING] http://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb-src [signed-by=$KEYRING] http://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update -y
-sudo apt install curl -y
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-apt install nodejs -y
+sudo apt-get install nodejs -y --allow-unauthenticated
+
+echo '==> Installing npm'
+sudo apt install libssl1.0-dev -y
+sudo apt install nodejs-dev -y
+sudo apt install node-gyp -y
+sudo apt install npm -y
 
 echo '==> Extract api artifact to /var/promotions-manager-api'
 mkdir $ARTIFACTS_PATH/drop
@@ -25,7 +45,7 @@ echo 'API_PORT='$API_PORT >> /etc/environment
 source /etc/environment
 
 echo '==> Install PM2, it provides an easy way to manage and daemonize nodejs applications'
-npm install -g pm2
+npm install -g pm2 -y
 
 echo '==> Start our api and configure as a daemon using pm2'
 cd /var/promotions-manager-api
